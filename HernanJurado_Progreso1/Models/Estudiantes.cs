55812cfbd10142f9e69ec142bab0cbfe8cbf2900
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-04-22 09:25:08 -0500

    Creacion Estudiantes

---
+++ HernanJurado_Progreso1/Models/Estudiantes.cs
@@ -0,0 +1,12 @@
+﻿namespace HernanJurado_Progreso1.Models
+{
+    public class Estudiantes
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public DateOnly? Fecha { get; set; }
+        public Boolean? Inscrito { get; set; }
+        public decimal? Matricula { get; set; }
+    }
+}
