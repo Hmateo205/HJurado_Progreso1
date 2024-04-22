822ea7b5ca46aca2651de2c1762035aa13b2311e
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-04-22 09:37:25 -0500

    Migrations

---
+++ HernanJurado_Progreso1/Migrations/20240422143139_HJuradoDB.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HernanJurado_Progreso1.Migrations
+{
+    /// <inheritdoc />
+    public partial class HJuradoDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carrera",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre_Carrera = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Campus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Semestre = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carrera", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estudiantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fecha = table.Column<DateOnly>(type: "date", nullable: true),
+                    Inscrito = table.Column<bool>(type: "bit", nullable: true),
+                    Matricula = table.Column<decimal>(type: "decimal(18,2)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudiantes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Carrera");
+
+            migrationBuilder.DropTable(
+                name: "Estudiantes");
+        }
+    }
+}
