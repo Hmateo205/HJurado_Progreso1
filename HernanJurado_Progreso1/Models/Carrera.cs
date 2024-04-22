0bba66b3da2963de4bd34afa461f809c19e40522
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-04-22 09:26:03 -0500

    Creracion Carrera

---
+++ HernanJurado_Progreso1/Models/Carrera.cs
@@ -0,0 +1,10 @@
+﻿namespace HernanJurado_Progreso1.Models
+{
+    public class Carrera
+    {
+        public int Id { get; set; }
+        public string Nombre_Carrera { get; set; }
+        public string? Campus { get; set; }
+        public int Semestre { get; set; }
+    }
+}
